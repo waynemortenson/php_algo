3c237d2c273d5e85af056b1ac701e0c2ca1c7a44
waynemortenson <waynemortenson@gmail.com>
2020-02-16 13:17:15 -0600

    counting chars in strings

---
+++ exsAndOhs.php
@@ -0,0 +1,17 @@
+<?php
+
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+$str = 'Ox';
+$lower = strtolower($str);
+if(substr_count($lower, "o") == substr_count($lower, "x"))
+{
+    return true;
+}
