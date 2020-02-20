dfa81fd1f4bfc2a00a2c4bf9dee3d49891894392
waynemortenson <waynemortenson@gmail.com>
2020-02-20 14:48:47 -0600

    squareSum refactored

--- squareSum.php
+++ squareSum.php
@@ -6,23 +6,30 @@
 For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
 */
+// $numbers = [1, 2, 3, 4, 5, 6, 7];
+// echo square_sum($numbers);
+
+// function square_sum($numbers)
+// {
+
+//     $array = array();
+
+
+//     $len = count($numbers);
+
+
+//     for ($i = 0; $i < $len; $i++) 
+//     {
+//         array_push($array, $numbers[$i] * $numbers[$i]);
+//     }
+
+
+//     return array_sum($array);
+// }
+
 $numbers = [1, 2, 3, 4, 5, 6, 7];
 echo square_sum($numbers);
-
 function square_sum($numbers)
 {
-
-    $array = array();
-
-
-    $len = count($numbers);
-
-
-    for ($i = 0; $i < $len; $i++) 
-    {
-        array_push($array, $numbers[$i] * $numbers[$i]);
-    }
-
-
-    return array_sum($array);
+    return array_sum(array_map(function($value){return $value * $value;}, $numbers));
 }
